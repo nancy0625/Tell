4527bcacbd0d0008fb62d046550432c2b1f4a5ab
nancy0625 <1308013168@qq.com>
2017-11-22 06:56:24 +0800

    Intent.ACTION_DIAL

--- app/src/main/java/cn/edu/gdmec/android/tell/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/tell/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
                 String telNum = telnum.getText().toString();
                 if (PhoneNumberUtils.isGlobalPhoneNumber(telNum)){
                     Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(//Uri.parse的方法返回的是一个URI类型，通过这个URI可以访问一个网络上或者是本地的资源
-                            //调用拨号盘
+                            //调用系统拨号盘
                             "tel://"+telNum
                     ));
                     MainActivity.this.startActivity(intent);
