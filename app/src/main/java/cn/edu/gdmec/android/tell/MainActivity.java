0a785675eecce18a7c991e6098e5ee3a57506005
nancy0625 <1308013168@qq.com>
2017-11-22 09:09:08 +0800

    add send message

--- app/src/main/java/cn/edu/gdmec/android/tell/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/tell/MainActivity.java
@@ -17,20 +17,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button bCall = (Button)findViewById(R.id.btn);
+        Button bSms = (Button)findViewById(R.id.btn_sms);
+        Button bContact = (Button)findViewById(R.id.btn_contact);
         bCall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EditText telnum = (EditText)findViewById(R.id.tel);
                 String telNum = telnum.getText().toString();
+                //拨打电话
                 if (PhoneNumberUtils.isGlobalPhoneNumber(telNum)){
                     Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(//Uri.parse的方法返回的是一个URI类型，通过这个URI可以访问一个网络上或者是本地的资源
                             //使用系统拨号盘
                             "tel://"+telNum
                     ));
+
                     MainActivity.this.startActivity(intent);
                 }else{
                     Toast.makeText(MainActivity.this,"号码不正确",Toast.LENGTH_LONG).show();
                 }
+
+
+
+            }
+        });
+            //发送短信
+        bSms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText telnum = (EditText)findViewById(R.id.tel);
+                String telNum = telnum.getText().toString();
+                //拨打电话
+                if (PhoneNumberUtils.isGlobalPhoneNumber(telNum)){
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(//Uri.parse的方法返回的是一个URI类型，通过这个URI可以访问一个网络上或者是本地的资源
+                            //使用系统拨号盘
+                            "smsto:"+telNum
+                    ));
+
+                    MainActivity.this.startActivity(intent);
+                }else{
+                    Toast.makeText(MainActivity.this,"号码不正确",Toast.LENGTH_LONG).show();
+                }
+
+
 
             }
         });
